9cc9c1ba955c4b02ea1bcddce1c92f754f797f13
pemafederer <108404833+pemafederer@users.noreply.github.com>
2023-12-21 16:33:30 +0100

    work in progress on gönner page

--- src/Goenner.jsx
+++ src/Goenner.jsx
@@ -30,8 +30,33 @@ return(
             
         </div>
         <div className="gönner-container">
+            <div className="gönner-picture">
             <div className="gönner-img">
-                <RModalImages />
+                <RModalImages
+                small="Savognin.jpg"
+                medium="Savognin.jpg"
+                hideDownloadButton="true"
+                hideRotateButton="true"
+                hideZoomButton="true"
+                 />
+            </div>
+            </div>
+            
+        </div>
+        <div className="gönner-container">
+            <div className="gönner-text">
+            <h3>
+                Mit einer Gönnermitgliedschaft untertstützen Sie mich finanziell und werden somit Teil meiner sportlichen Laufbahn.
+                Sie sorgen mit Ihrem Gönnerbeitrag direkt für die Finanzierung der Reise/Unterkunftskosten, Kosten für Trainingslager usw.
+            </h3>
+            <h3>
+                Am Ende der Saison erwartet Sie als Gönner ein kleines Geschenk als Dankeschön für die wertvolle Unterstützung. Zudem erhalten Sie 
+             jeden Rennbericht per Email und haben die Möglichkeiten meinen Rennkalender auf Outlook zu abonnieren.
+            </h3>
+            <h3>
+               Die Mitgliedschaft ist unbefristet und Ihrerseits gibt es keine Verpflichtungen. Zudem entscheiden Sie frei über den Betrag und die Laufdauer. 
+                Ich würde mich über jeglichen Beitrag freuen.
+            </h3>
             </div>
         </div>
 
