71d0254d0253eac1947c7f4354f25caf0e8c1e83
pemafederer <108404833+pemafederer@users.noreply.github.com>
2023-12-31 15:55:41 +0100

    uninstall vercel speed insights

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout(){
 
     return(
         <div>
-            <SpeedInsights >
             <Navbar />
             <main>
                 <Outlet />
             </main>
             <Footer />
-            </SpeedInsights>
         </div>
     )
 }
