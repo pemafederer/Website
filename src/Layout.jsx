1c4c8446f4680b618e3450589d7173e24995aa1a
pemafederer <108404833+pemafederer@users.noreply.github.com>
2023-12-31 15:52:06 +0100

    </SpeedInsights>

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout(){
 
     return(
         <div>
+            <SpeedInsights >
             <Navbar />
             <main>
                 <Outlet />
             </main>
             <Footer />
+            </SpeedInsights>
         </div>
     )
 }
