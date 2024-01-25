19693d189b307d623f89381e38272cc6371210a0
pemafederer <108404833+pemafederer@users.noreply.github.com>
2024-01-25 16:03:24 +0100

    toastify

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,9 @@
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 console.log("hat Contact geöffnet")
 
@@ -11,19 +14,38 @@ const Contact = () => {
   const telefon = useRef(null);
   const email = useRef(null);
   const message = useRef(null);
+  const [showPopup, setShowPopup] = useState(false);
+
 
 
 
 
   const sendEmail = (e) => {
+    <p> Vielen dank für ihre Email</p>
     e.preventDefault();
 
     emailjs.sendForm('service_sv7fw1o', 'template_y2utozs', form.current, 'c0ZobUEMjmj9hJaPO')
       .then((result) => {
         console.log(result.text);
         console.log("message sent")
+        toast.success('Vielen Dank für Ihre Email!', {
+          position: 'top-right',
+          autoClose: 3000, // Close the toast after 3 seconds
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
       }, (error) => {
         console.log(error.text);
+        toast.error('Fehler beim Senden der Email!', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
       });
       console.log(form.current.value)
 
@@ -36,16 +58,25 @@ const Contact = () => {
       /*<label htmlFor="from_name">Name</label>*/
   }
   return (
-    <div className="contact-container">
+    <><div className="contact-container">
       <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name="from_surname" placeholder="Vorname" ref={vorname}/>
-        <input type="text" name="from_name" placeholder="Nachname" ref={nachname}/>
-        <input type="text" name="telefon" placeholder="Telefon" ref={telefon}/>
-        <input type="email" name="user_email" placeholder="Email-Adresse" ref={email}/>
+        <input type="text" name="from_surname" placeholder="Vorname" ref={vorname} />
+        <input type="text" name="from_name" placeholder="Nachname" ref={nachname} />
+        <input type="text" name="telefon" placeholder="Telefon" ref={telefon} />
+        <input type="email" name="user_email" placeholder="Email-Adresse" ref={email} />
         <textarea name="message" placeholder="Nachricht" s ref={message}></textarea>
         <input type="submit" value="Send" />
       </form>
-    </div>
+    </div><ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover /></>
 
 
   )
