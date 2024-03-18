df3b796b2721ea37d916d5eb7ca23a1503481218
pemafederer <108404833+pemafederer@users.noreply.github.com>
2024-03-18 20:00:40 +0100

    add new rennbericht

--- src/data-rennberichte.js
+++ src/data-rennberichte.js
@@ -1,6 +1,21 @@
 export default [
+
 {
     id: 0,
+    date: '18.03.2024',
+    title: "Solider Saisonstart",
+    subtitle:" Ich konnte meine Saison mit einem soliden Rennen beim CIC Swissbikecup in Gränichen. Der Regen in den Vortagen machte die Strecke extrem rutschig und tief. Nach einem guten Start konnte ich immer sauber fahren und bin zufrieden mit P.7 bei den Amateure Masters." ,
+    img: "Graenichen2024.jpg",
+    img_container_1: "Graenichen2024_s.jpg",
+    img_container_2: "Graenichen2024_s.jpg",
+    img_container_3: "Graenichen2024_2.jpg",
+    fulltext: "Mit viel Vorfreude startete ich am vergangenen Wochenende in meine Rennsaison beim Swissbikecup in Gränichen. Zwei Trainingslager in Spanien und viele Trainingsstunden in den Beinen, war ich bestens auf den Saisonauftakt vorbereitet.Die Strecke in Gränichen ist bekannt für ihre abwechslungsreiche und technisch anspruchsvolle Führung durch Kiesgruben und einen Bike-Lehrpfad. In diesem Jahr wurde die Strecke leicht verändert und eine neue Abfahrt eingebaut. Die nassen Bedingungen der vergangenen Woche hatten den Boden jedoch aufgeweicht und die neue Abfahrt war extrem rutschig und tief. Pünktlich um 8:45 Uhr startete ich bei den Amateuren Masters ins Rennen. Dank meiner Platzierung in der letzten Saison konnte ich als Sechster ins Rennen gehen. Den Start meisterte ich gut und fand mich nach der ersten Abfahrt auf Platz 10 wieder. In der zweiten Runde konnte ich einige Plätze gutmachen, bevor mir in einer Abfahrt ein kleiner Fehler unterlief. Dies kostete mich wertvolle Zeit und den Anschluss an die Top 6. In der letzten Runde mobilisierte ich noch einmal alle Kräfte und konnte einen Platz gutmachen. Mit dem 7. Platz bin ich für den Saisonauftakt zufrieden, sehe aber gleichzeitig noch viel Verbesserungspotenzial. Nächste Woche geht es beim Swissbikecup in Rivera weiter. Ich freue mich auf das erste U23-Rennen und bin gespannt, wie ich mich gegen die starke Konkurrenz behaupten kann.",
+
+
+
+},
+{
+    id: 1,
     title: "Swissbikecup U19 Gesamtsieg ",
     subtitle: "Nach einem Massensturz am Start, musste ich das Feld von hinten aufrollen. Dies gelang mir gut und ich befand mich nach Rennhälfte auf P.7. Leider konnte ich den Gap zum Trio um P.4 nie ganz schliessen. Mit dem 7ten Platz konnte ich souverän die Gesamtwertung des Swissbikecup's gewinnen. ",
     img: "gstaad.WebP",
@@ -10,7 +25,7 @@ export default [
     fulltext: "Heute stand das letzte Rennen des Swissbikecups und für mich auch das letzte Rennen einer langen Saison an. Ich setzte mir als Ziel nochmals eine gute Leistungs zum Saisonabschluss zu zeigen. Die Strecke in Gstaad ist sehr speziell da sie auf dem Flugplatz startet und die ersten 2km flach sind. Die Runde hat zwei Anstiege die teilweise auch sehr steil sind und die Abfahrten führen über Wiesen oder durch eine Kiesgrube. Durch den Regen in den Vortagen war die Strecke sehr nass und vorallem die Wiesenabfahrt extrem rutschig. Am Vortag waren gewisse Stellen schon unfahrbar und dies war auch am Sonntag der Fall. Ich erwischte einen sehr ungünstigen Start, da ich 200m nach dem Start in einen grossen Massensturz verwickelt wurde. Ich stieg jedoch schnell wieder auf und fuhr weiter trotz beachtlicher Schmerzen am Mittelfinger und im Rücken. Auch mit dem Gedanken an die Gesamtwertung. In den darauffolgenden Runden konnte ich mich nach vorne arbeiten und fühlte mich auch sehr gut. Die Bedingungen lagen mir und es machte sehr viel Spass bei den rutschigen Bedingungen zu fahren. Schlussendlich beendete ich das Rennen auf dem guten siebten Platz und konnte mir somit den Sieg in der Gesamtwertung der Junioren sichern😊. Mit diesem Erfolg bin ich extrem zufrieden und ist ein perfekter Abschluss einer super Saison."
 },
 {
-    id: 1,
+    id: 2,
     title: "Junior Series Basel",
     subtitle: " Nach der Sommerpause startete die zweite Saisonhälfte mit der Junior Series in Basel. Ich fuhr lange in der Verfolgergruppe(P.3 - P.9), verlor jedoch im letzten Anstieg den Anschluss. Nach einem super Rennen fuhr ich auf dem 9ten Platz ins Ziel und egalisierte damit mein bestes Resultat an einer Junior Series deutlich. ",
     img: "Basel.WebP",
@@ -21,7 +36,7 @@ export default [
     
 },
 {
-    id: 2,
+    id: 3,
     date: '13.12.2023',
     title: "SM in Crans-Montana",
     subtitle: "Das Saisonhighlight stand an. Zum ersten Mal wurde in Crans-Montana ein XCO Rennnen ausgetragen. Die Strecke war extrem steil und hatte viele kurze knackige Anstiege sowie auch technische Abfahrten. Das Rennen verlief okay und schlussendlisch beendete ich die SM auf P.14, nach einer zu schnellen Startrunde",
@@ -32,7 +47,7 @@ export default [
     fulltext: "Heute stand das Saisonhighlight mit der Mountainbike Schweizermeisterschaft in Crans-Montana statt. Die Form passte und ich sah sehr zuversichtlich auf das Rennen voraus. Die Strecke war brandneu und physisch sehr anspruchsvoll. Gewisse Teile der Strecke waren künstlich erstellt worden im Hinblick auf die WM 2025. Zudem liegt Crans-Montana auf rund 1500 mü.M., was das ganze Rennen noch anstrengender machte. Die Anstiege sowie auch die Abfahrte waren alle sehr steil und zudem war die ganze Strecke staubtrocken. Mein Start gelang mir sehr gut und ich war zwischenzeitlich auf P.14. Ich merkte jedoch sehr schnell, dass ich zu schnell gestartet bin. In den folgenden zwei grossen Runden büsste ich für meinen Starteffort und brauchte ein bisschen bis ich wieder in den Rhytmus reinkam. In den letzten drei Runden konnte ich nochmals ein bisschen aufdrehen und verlor den Sprint um P.13 und wurde 14ter. Alles in allem war mein Rennen solid aber auch nicht mehr."
 },
 {
-    id: 3,
+    id: 4,
     title: "Podestplatz beim XCC ",
     subtitle: "Am Auffahrtssonntag fand in der Innenstadt von Lugano das XCC Rennnen des Swissbikecups statt. Die Strecke führte durch die Altstadt und beinhaltete einen kurzen Aufstieg und eine darauffolgende Treppenabfahrt. Nach einem soliden Rennen konnte ich auf P.5 ins Ziel fahren. ",
     img: "Lugano_XCC.WebP",
@@ -44,7 +59,7 @@ export default [
     fulltext: "Pema Federer, im Towers Sports VC Eschenbach Race Team Dress unterwegs, gelang der Start ins Junioren Rennen wie gewünscht. Ende der Startrunde fand sich Federer auf dem siebten Zwischenrang wieder. Während der gesamten Renndauer konnte sich Pema Federer stetig steigern. Jedoch wurde er bei Rennhälfte jäh zurückgeworfen. Federer zu diesem Rennmoment: «Ende der vierten Runde merkte ich, dass der Pneu Luft verliert. Leider erst, als ich schon bei der Techzone vorbei war. Darum musste ich fast eine ganze Runde mit sehr wenig Luft fahren. Ich verlor dafür einige Plätze.» In der letzten Runde konnte Federer, wieder mit fahrtüchtigem Bike unterwegs, nochmals Plätze gut machen. Schlussendlich beendete er das Rennen auf dem 13. Rang. Pema zum Rennen: «Trotz dem Endresultat bin ich zufrieden mit meiner ersten Rennhälfte und nehme positives daraus. So bin ich zuversichtlich für die Schweizermeisterschaften in zwei Wochen.Auch Federer kurvte am Sonntag beim Shortrace Rennen durch die Altstadt Luganos. Die Strecke war sehr schnell und hatte einen kurzen aber knackigen Anstieg mit einer darauffolgenden Treppenabfahrt. Federer startete gut ins Rennen. Während der gesamten Dauer war der Wettkampf sehr taktisch. Vor allem auf der langen Gerade von Start und Ziel wurde bewusst taktiert und Tempo rausgenommen. Pema Federer zum Rennen: «Ich konnte mich fast während dem ganzen Rennen an der Spitze halten und fuhr aktiv mit. Leider fehlte mir in den letzten zwei Runden die letzte Kraft, um den Sieg zu kämpfen zu können. Schlussendlich reichte es für den guten 5. Platz. Ich bin zufrieden mit meinem Rennen obwohl durch besseres taktisches Verhalten eine bessere Platzierung drin gelegen wäre. Zudem freue ich mich über mein erstes Swissbikecup Podest seit mehreren Jahren.",
 },
 {
-    id: 4,
+    id: 5,
     title: "Erste Junior Series im Ausland",
     subtitle: "Aufgrund meines guten Rennens an der Junior Series Rivera, bekam ich einen Nationalmannschafts Startplatz für die Junior Series in Haiming. Die Strecke war extrem technisch und physisch sehr anspruchsvoll.  Mein Rennen verlief ganz solide, auch wenn ich mir mehr erhoffte.  ",
     img: "Haiming.WebP",
@@ -54,7 +69,7 @@ export default [
     fulltext: "Durch mein sehr gutes Rennen in Rivera bekam ich von der Nationalmannschaft einen Startplatz an der Junior Series in Haiming. Ich freute mich sehr über diese Möglichkeit, da es für mich das erste Mal war im Dress der Nationalmannschaft zu starten. Wie in Rivera einige Wochen zuvor war das Rennen in Haiming auch eine Junior Series. Ein Juniorenrennen mit dem höchsten UCI-Status. Deshalb war das Feld auch extrem gut und international besetzt. Die Strecke in Haiming ist bekannt für die technischen Schwirigkeiten. Durch den Regen in der Vorwoche wurden die vielen Wurzelpassagen extrem rutschig und auch die Anstiege mussten teilweise geschoben werden. Als wir am Vortag die Strecke besichtigten fühlte ich mich jedoch gut und sehr sicher. Das Rennen startete ich von P.61/92 aus, da ich noch weniger als 20 UCI Punkte besitzte. Mein Start verlief gut, leider stürzte ich noch in der ersten Runde und verlor dadurch noch einige paar Plätze. In den folgenden Runden konnte ich immer wieder Fahrer überholen dies auch weil ich technisch sauber fuhr. Schlussendlich konnte ich das Rennen auf P.38/92 beenden, als 9ter Schweizer. Alles in allem bin ich zufrieden mit meine Rennen und konnte ein gutes Rennen im Dress der Nationalmannschaft zeigen. "
 },
 {
-    id: 5,
+    id: 6,
     title: "Erste UCI Punkte in Rivera",
     subtitle: "Nach einem verpatzten Saisonstart in Gränichen, konnte ich an der Junior Series in Rivera ein super Rennen zeigen und zu meinen ersten UCI Punkten fahren. Ich kamm am Start sehr gut weg und konnte von da an mein Rennen voll durchziehen. Schlussendlich kam ich auf P.21 ins Ziel. ",
     img: "Rivera_Start.WebP",
